0c7a44cf32ffd08b4d012f42a352e4c889c4552e
NazarBylen <nazar.bylen.ir.2021@lpnu.ua>
2022-05-24 15:26:55 +0300

    refactored base class as abstract class

--- animals.py
+++ animals.py
@@ -1,58 +1,63 @@
-class animal:
-    name = 'Slavik'
+from abc import ABC, abstractmethod
+
+class animal(ABC):
 
     def __init__(self, name: str):
-        self.name = name
+        self._name = name
+
+    @abstractmethod
+    def print_name_and_2nd_arg(self):
+        pass
 
 
 class mammals(animal):
     height = ''
 
     def __init__(self, name: str, height: float):
-        super().__init__(name)
+        self.name = name
         self.height = height
 
-    def print_name_and_height(self):
+    def print_name_and_2nd_arg(self):
         print(f'name : {self.name}, height : {self.height}')
 
 
 class bird(animal):
     age = 0
 
-    def __init__(self, age: int):
-        self.name = animal.name
+    def __init__(self, _name : str, age: int):
+        super().__init__(_name)
         self.age = age
 
-    def print_name_and_age(self):
-        print(f'name : {self.name}, age : {self.age}')
+    def print_name_and_2nd_arg(self):
+        print(f'name : {self._name}, age : {self.age}')
 
 
 class insect(animal):
-    def __init__(self, weight: float):
-        self.name = animal.name
+    def __init__(self, _name : str, weight: float):
+        super().__init__(_name)
         self.weight = weight
 
-    def print_name_and_weight(self):
-        print(f'name : {self.name}, weight : {self.weight}')
+    def print_name_and_2nd_arg(self):
+        print(f'name : {self._name}, weight : {self.weight}')
 
 
 class reptile(animal):
     legs = 0
 
-    def __init__(self, legs: int):
-        self.name = animal.name
+    def __init__(self, _name : str, legs: int):
+        super().__init__(_name)
         self.legs = legs
 
-    def print_name_and_legs(self):
-        print(f'name : {self.name}, number of legs : {self.legs}')
+    def print_name_and_2nd_arg(self):
+        print(f'name : {self._name}, number of legs : {self.legs}')
 
 
 class fish(animal):
     type_of_skin = ''
 
-    def __init__(self, type_of_skin: str):
-        self.name = animal.name
+    def __init__(self, _name : str, type_of_skin: str):
+        super().__init__(_name)
         self.type_of_skin = type_of_skin
 
-    def print_name_and_type_of_skin(self):
-        print(f'name : {self.name}, type of skin : {self.type_of_skin}')
+    def print_name_and_2nd_arg(self):
+        print(f'name : {self._name}, type of skin : {self.type_of_skin}')
--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 from animals import *
 
-animal = animal('Slavik')
 mammals = mammals('Lyoxa', 45.6)
-bird = bird(3)
-insect = insect(0.5)
-reptile = reptile(4)
-fish = fish('dirty skin (idk what types of skin fish have))')
+bird = bird('Oleksandr', 3)
+insect = insect('Oleksiy', 0.5)
+reptile = reptile('Oleksandr', 4)
+fish = fish('Oleksandr', 'dirty skin (idk what types of skin fish have))')
 
 def print_animals():
-    print(animal.name)
-    mammals.print_name_and_height()
-    bird.print_name_and_age()
-    insect.print_name_and_weight()
-    reptile.print_name_and_legs()
-    fish.print_name_and_type_of_skin()
+    mammals.print_name_and_2nd_arg()
+    bird.print_name_and_2nd_arg()
+    insect.print_name_and_2nd_arg()
+    reptile.print_name_and_2nd_arg()
+    fish.print_name_and_2nd_arg()
 
 
 def main():
